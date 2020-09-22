b34047cf5386f8faef6f12f628e18590510c72b5
Matt Hicks <matt@courio.com>
2020-09-22 12:04:21 -0700

    Playing around with the model structure

--- build.sbt
+++ build.sbt
@@ -7,5 +7,7 @@ scalaVersion := "2.13.3"
 resolvers += "yahoo-bintray" at "https://yahoo.bintray.com/maven"
 
 libraryDependencies ++= Seq(
+	"com.outr" %% "lucene4s" % "1.10.0",
+	"io.youi" %% "youi-core" % "0.13.16",
 	"com.oath.halodb" % "halodb" % "0.5.3"
 )
--- src/main/scala/testdb/Test.scala
+++ src/main/scala/testdb/Test.scala
@@ -1,8 +1,11 @@
 package testdb
 
 import com.oath.halodb.{HaloDB, HaloDBOptions}
+import com.outr.lucene4s.query.QueryBuilder
+import io.youi.Unique
 
 import scala.jdk.CollectionConverters._
+import scala.util.matching.Regex
 
 object Test {
   def main(args: Array[String]): Unit = {
@@ -24,6 +27,17 @@ object Test {
     }
   }
 
+  def test(): Unit = {
+    val iterator: DBIterator[User] = Database.users
+      .limit(100)
+      .offset(10)
+      .filter(User.name === "Matt")
+      .filter(User.age >= 21)
+      .filter(User.name fuzzy "mt")
+      .sort(User.age.desc)
+      .query()
+  }
+
   implicit class BytesExtras(val array: Array[Byte]) extends AnyVal {
     def string: String = new String(array, "UTF-8")
   }
@@ -31,4 +45,68 @@ object Test {
   implicit class StringExtras(val s: String) extends AnyVal {
     def bytes: Array[Byte] = s.getBytes("UTF-8")
   }
+}
+
+case class Id[T](collection: String, value: String = Unique(length = 32))
+
+object Id {
+  private val IdRegex: Regex = """(.+)[/](.+)""".r
+
+  def toString[T](id: Id[T]): String = s"${id.collection}/${id.value}"
+  def fromString[T](s: String): Id[T] = s match {
+    case IdRegex(collection, value) => Id[T](collection, value)
+  }
+}
+
+trait Document[D <: Document[D]] {
+  val id: Id[D]
+}
+
+trait DocumentModel[D <: Document[D]] {
+  def field[T](name: String): Field[T] = new Field[T](name)
+}
+
+case class Filter()
+
+class Field[T](val name: String) extends AnyVal
+
+object Field {
+  def apply[T](name: String): Field[T] = new Field[T](name)
+}
+
+case class User(name: String, age: Int, id: Id[User]) extends Document[User]
+
+object User extends DocumentModel[User] {
+  val name: Field[String] = field("name")
+  val age: Field[Int] = field("age")
+}
+
+trait Sort {
+  def fieldName: String
+  def reverse: Boolean
+}
+
+trait QueryBuilder[T] {
+  def limit(limit: Int): QueryBuilder[T]
+  def offset(offset: Int): QueryBuilder[T]
+  def filter(filter: Filter): QueryBuilder[T]
+  def sort(sort: Sort): QueryBuilder[T]
+  def query(): DBIterator[T]
+}
+
+trait DBIterator[T] extends Iterator[T] {
+  def total: Int
+  def position: Int
+}
+
+trait Collection[D <: Document[D]] extends QueryBuilder[D] {
+  def model: DocumentModel[D]
+}
+
+trait DB {
+  protected def collection[D <: Document[D]]: Collection[D] = ???
+}
+
+object Database extends DB {
+  val users: Collection[User] = collection[User]
 }
