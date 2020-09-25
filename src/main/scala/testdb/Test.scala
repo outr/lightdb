e3733219074822927f02ca609783bb5b252e78d8
Matt Hicks <matt@courio.com>
2020-09-24 20:02:16 -0700

    Playing around with the model structure

--- src/main/scala/testdb/Test.scala
+++ src/main/scala/testdb/Test.scala
@@ -1,10 +1,13 @@
 package testdb
 
+import java.io.File
+
 import com.oath.halodb.{HaloDB, HaloDBOptions}
-import com.outr.lucene4s.query.QueryBuilder
+import io.circe.{Json, Printer}
 import io.youi.Unique
 
 import scala.jdk.CollectionConverters._
+import scala.util.Try
 import scala.util.matching.Regex
 
 object Test {
@@ -33,8 +36,8 @@ object Test {
       .offset(10)
       .filter(User.name === "Matt")
       .filter(User.age >= 21)
-      .filter(User.name fuzzy "mt")
-      .sort(User.age.desc)
+//      .filter(User.name fuzzy "mt")
+//      .sort(User.age.desc)
       .query()
   }
 
@@ -45,7 +48,14 @@ object Test {
   implicit class StringExtras(val s: String) extends AnyVal {
     def bytes: Array[Byte] = s.getBytes("UTF-8")
   }
+
+  implicit class IntFieldExtras(val f: Field[Int]) extends AnyVal {
+    def >=(value: Int): Filter = Filter.ValueComparison(f, Operator.GreaterOrEqual, value)
+  }
 }
+
+// Create "core" for cross-platform no-dependencies code
+// Create "database" for database code (JVM-only)
 
 case class Id[T](collection: String, value: String = Unique(length = 32))
 
@@ -63,15 +73,11 @@ trait Document[D <: Document[D]] {
 }
 
 trait DocumentModel[D <: Document[D]] {
-  def field[T](name: String): Field[T] = new Field[T](name)
+  def field[T](name: String, stored: Boolean = true): Field[T] = Field[T](name, stored)
 }
 
-case class Filter()
-
-class Field[T](val name: String) extends AnyVal
-
-object Field {
-  def apply[T](name: String): Field[T] = new Field[T](name)
+case class Field[T](name: String, stored: Boolean) {
+  def ===(value: T): Filter = Filter.ValueComparison(this, Operator.Equals, value)
 }
 
 case class User(name: String, age: Int, id: Id[User]) extends Document[User]
@@ -79,6 +85,22 @@ case class User(name: String, age: Int, id: Id[User]) extends Document[User]
 object User extends DocumentModel[User] {
   val name: Field[String] = field("name")
   val age: Field[Int] = field("age")
+}
+
+sealed trait Filter
+
+object Filter {
+  case class ValueComparison[T](field: Field[T], operator: Operator, value: T) extends Filter
+}
+
+sealed trait Operator
+
+object Operator {
+  case object Equals extends Operator
+  case object Greater extends Operator
+  case object GreaterOrEqual extends Operator
+  case object Less extends Operator
+  case object LessOrEqual extends Operator
 }
 
 trait Sort {
@@ -104,9 +126,71 @@ trait Collection[D <: Document[D]] extends QueryBuilder[D] {
 }
 
 trait DB {
+  val directory: Option[File]
+
+  lazy val store: Store = directory match {
+    case Some(dir) => new HaloDBStore(new File(dir, "store"))
+    case None => new MapStore
+  }
+
   protected def collection[D <: Document[D]]: Collection[D] = ???
 }
 
 object Database extends DB {
+  override val directory: Option[File] = None
+
   val users: Collection[User] = collection[User]
+}
+
+trait Store {
+  def get(key: String): Option[Json]
+  def apply(key: String): Json
+  def update(key: String, value: Json): Unit
+  def close(): Unit
+}
+
+class HaloDBStore(directory: File) extends Store {
+  private lazy val options: HaloDBOptions = {
+    val o = new HaloDBOptions
+    o.setBuildIndexThreads(8)
+    o
+  }
+  private lazy val db = HaloDB.open(directory, options)
+
+  override def get(key: String): Option[Json] = Try(apply(key)).toOption
+
+  override def apply(key: String): Json = {
+    val keyBytes = key.getBytes("UTF-8")
+    val valueBytes = db.get(keyBytes)
+    val valueString = new String(valueBytes, "UTF-8")
+    io.circe.parser.parse(valueString) match {
+      case Left(failure) => throw failure
+      case Right(json) => json
+    }
+  }
+
+  override def update(key: String, value: Json): Unit = {
+    val keyBytes = key.getBytes("UTF-8")
+    val valueString = value.printWith(Printer.spaces2)
+    val valueBytes = valueString.getBytes("UTF-8")
+    db.put(keyBytes, valueBytes)
+  }
+
+  override def close(): Unit = db.close()
+}
+
+class MapStore extends Store {
+  private var map = Map.empty[String, Json]
+
+  override def get(key: String): Option[Json] = map.get(key)
+
+  override def apply(key: String): Json = map(key)
+
+  override def update(key: String, value: Json): Unit = synchronized {
+    map += key -> value
+  }
+
+  override def close(): Unit = synchronized {
+    map = Map.empty
+  }
 }
