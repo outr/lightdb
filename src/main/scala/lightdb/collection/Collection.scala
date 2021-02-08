2c9693d89c4019bf7d46777793e212d6512989fd
Matt Hicks <matt@matthicks.com>
2021-02-07 20:21:38 -0800

    Switched to using cats-effect

--- src/main/scala/lightdb/collection/Collection.scala
+++ src/main/scala/lightdb/collection/Collection.scala
@@ -1,17 +1,16 @@
 package lightdb.collection
 
+import cats.effect.IO
 import lightdb.data.DataManager
 import lightdb.{Id, LightDB}
-
-import scala.concurrent.{ExecutionContext, Future}
 
 trait Collection[T] {
   protected def db: LightDB
   protected def dataManager: DataManager[T]
 
-  def get(id: Id[T])(implicit ec: ExecutionContext): Future[Option[T]] = db.store.get(id).map(_.map(dataManager.fromArray))
-  def put(id: Id[T], value: T)(implicit ec: ExecutionContext): Future[T] = db.store.put(id, dataManager.toArray(value)).map(_ => value)
-  def modify(id: Id[T])(f: Option[T] => Option[T])(implicit ec: ExecutionContext): Future[Option[T]] = {
+  def get(id: Id[T]): IO[Option[T]] = db.store.get(id).map(_.map(dataManager.fromArray))
+  def put(id: Id[T], value: T): IO[T] = db.store.put(id, dataManager.toArray(value)).map(_ => value)
+  def modify(id: Id[T])(f: Option[T] => Option[T]): IO[Option[T]] = {
     var result: Option[T] = None
     db.store.modify(id) { bytes =>
       val value = bytes.map(dataManager.fromArray)
@@ -19,5 +18,5 @@ trait Collection[T] {
       result.map(dataManager.toArray)
     }.map(_ => result)
   }
-  def delete(id: Id[T])(implicit ec: ExecutionContext): Future[Unit] = db.store.delete(id)
+  def delete(id: Id[T]): IO[Unit] = db.store.delete(id)
 }
