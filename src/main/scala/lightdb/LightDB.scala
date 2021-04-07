5a8055d522be63eec6685de49a2db30106fc329f
Matt Hicks <matt@courio.com>
2021-04-07 16:20:23 -0700

    Continued prototyping

--- src/main/scala/lightdb/LightDB.scala
+++ src/main/scala/lightdb/LightDB.scala
@@ -1,18 +1,8 @@
 package lightdb
 
 import cats.effect.IO
-import lightdb.collection.Collection
-import lightdb.data.DataManager
 import lightdb.store.ObjectStore
 
-import scala.concurrent.ExecutionContext
-
-class LightDB(val store: ObjectStore) { thisDb =>
-  def collection[T](implicit dm: DataManager[T]): Collection[T] = new Collection[T] {
-    override protected def db: LightDB = thisDb
-
-    override protected def dataManager: DataManager[T] = dm
-  }
-
+class LightDB(val store: ObjectStore) {
   def dispose(): IO[Unit] = store.dispose()
 }
