97c12f362d288d6fd68cda4687b78f594a682ada
Matt Hicks <matt@matthicks.com>
2021-04-07 19:10:34 -0700

    Updates

--- src/main/scala/lightdb/LightDB.scala
+++ src/main/scala/lightdb/LightDB.scala
@@ -1,8 +1,23 @@
 package lightdb
 
 import cats.effect.IO
+import cats.instances.list._
+import cats.syntax.parallel._
 import lightdb.store.ObjectStore
 
 class LightDB(val store: ObjectStore) {
-  def dispose(): IO[Unit] = store.dispose()
+  private var _collections = List.empty[Collection[_]]
+
+  def collection[T](mapping: ObjectMapping[T]): Collection[T] = synchronized {
+    val c = Collection[T](this, mapping)
+    _collections = _collections ::: List(c)
+    c
+  }
+
+  def dispose(): IO[Unit] = for {
+    _ <- _collections.map(_.dispose()).parSequence
+    _ <- store.dispose()
+  } yield {
+    ()
+  }
 }
