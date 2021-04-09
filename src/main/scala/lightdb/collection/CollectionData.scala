36bcf4749eef76e76e361cb4a5b9828ffac20d48
Matt Hicks <matt@courio.com>
2021-04-08 17:39:11 -0700

    Continued prototyping

--- src/main/scala/lightdb/collection/CollectionData.scala
+++ src/main/scala/lightdb/collection/CollectionData.scala
@@ -9,22 +9,22 @@ case class CollectionData[D <: Document[D]](collection: Collection[D]) {
 
   protected def dataManager: DataManager[D] = collection.mapping.dataManager
 
-  def get(id: Id[D]): IO[Option[D]] = db.store.get(id).map(_.map(dataManager.fromArray))
+  def get(id: Id[D]): IO[Option[D]] = collection.store.get(id).map(_.map(dataManager.fromArray))
 
   def apply(id: Id[D]): IO[D] = get(id).map(_.getOrElse(throw new RuntimeException(s"Not found by id: $id")))
 
-  def put(id: Id[D], value: D): IO[D] = db.store.put(id, dataManager.toArray(value)).map(_ => value)
+  def put(id: Id[D], value: D): IO[D] = collection.store.put(id, dataManager.toArray(value)).map(_ => value)
 
   def modify(id: Id[D])(f: Option[D] => Option[D]): IO[Option[D]] = {
     var result: Option[D] = None
-    db.store.modify(id) { bytes =>
+    collection.store.modify(id) { bytes =>
       val value = bytes.map(dataManager.fromArray)
       result = f(value)
       result.map(dataManager.toArray)
     }.map(_ => result)
   }
 
-  def delete(id: Id[D]): IO[Unit] = db.store.delete(id)
+  def delete(id: Id[D]): IO[Unit] = collection.store.delete(id)
 
-  def commit(): IO[Unit] = db.store.commit()
+  def commit(): IO[Unit] = collection.store.commit()
 }
