3c2309409370e059b69ee7d3faf3e5b4f5e0c329
Matt Hicks <matt@courio.com>
2021-04-10 11:25:41 -0700

    Completed removed Lucene

--- src/main/scala/lightdb/index/package.scala
+++ src/main/scala/lightdb/index/package.scala
@@ -1,18 +1,18 @@
 package lightdb
 
-import lightdb.field.{Field, FieldFeature}
+import lightdb.field.Field
+import lightdb.index.feature.IndexFeature
 
 package object index {
   implicit class StringFieldExtras[T](field: Field[T, String]) {
-    def indexed: Field[T, String] = field.withFeature(StringIndexed)
+    def indexed: Field[T, String] = field.withFeature(IndexFeature.String)
   }
   implicit class IntFieldExtras[T](field: Field[T, Int]) {
-    def indexed: Field[T, Int] = field.withFeature(IntIndexed)
+    def indexed: Field[T, Int] = field.withFeature(IndexFeature.Int)
   }
 }
 
-sealed trait IndexFeature extends FieldFeature
 
-object StringIndexed extends IndexFeature
 
-object IntIndexed extends IndexFeature
+
+
