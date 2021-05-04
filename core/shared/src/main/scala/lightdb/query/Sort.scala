0520f89319a13e2a13bf53e014118875a3407a7f
Matt Hicks <matt@matthicks.com>
2021-05-04 13:05:56 -0700

    Updates and improvements to searching functionality.

--- core/shared/src/main/scala/lightdb/query/Sort.scala
+++ core/shared/src/main/scala/lightdb/query/Sort.scala
@@ -1,3 +1,11 @@
 package lightdb.query
 
+import lightdb.field.Field
+
 sealed trait Sort
+
+object Sort {
+  case object BestMatch extends Sort
+  case object IndexOrder extends Sort
+  case class ByField[T, F](field: Field[T, F], reverse: Boolean = false) extends Sort
+}
