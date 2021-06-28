4c3161c927a9f57288f70aba27c4ff28ad735fca
Matt Hicks <matt@matthicks.com>
2021-06-28 08:36:38 -0700

    Benchmarking and cleanup

--- core/shared/src/main/scala/lightdb/ObjectMapping.scala
+++ core/shared/src/main/scala/lightdb/ObjectMapping.scala
@@ -4,6 +4,8 @@ import lightdb.data.DataManager
 import lightdb.field.Field
 
 trait ObjectMapping[D <: Document[D]] { om =>
+  type FD[F] = Field[D, F]
+
   private var _fields: List[Field[D, _]] = Nil
 
   val _id: Field[D, Id[D]] = field("_id", _._id)
@@ -13,13 +15,13 @@ trait ObjectMapping[D <: Document[D]] { om =>
   def dataManager: DataManager[D]
 
   object field {
-    def get[F](name: String): Option[Field[D, F]] = _fields.find(_.name == name).map(_.asInstanceOf[Field[D, F]])
+    def get[F](name: String): Option[FD[F]] = _fields.find(_.name == name).map(_.asInstanceOf[Field[D, F]])
 
-    def apply[F](name: String, getter: D => F): Field[D, F] = {
+    def apply[F](name: String, getter: D => F): FD[F] = {
       replace(Field[D, F](name, getter, Nil, om))
     }
 
-    def replace[F](field: Field[D, F]): Field[D, F] = om.synchronized {
+    def replace[F](field: Field[D, F]): FD[F] = om.synchronized {
       _fields = _fields.filterNot(_.name == field.name) ::: List(field)
       field
     }
