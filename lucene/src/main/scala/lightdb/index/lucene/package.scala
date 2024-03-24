87cf81245c6ee687f04b81de03dfbe48b6ebe4c2
Matt Hicks <matt@matthicks.com>
2024-03-23 20:15:22 -0500

    Prototyped direct Lucene

--- lucene/src/main/scala/lightdb/index/lucene/package.scala
+++ lucene/src/main/scala/lightdb/index/lucene/package.scala
@@ -1,22 +1,22 @@
-package lightdb.index
-
-import lightdb.{Document, Id}
-import lightdb.field.Field
-
-package object lucene {
-  private object _idSupport extends StringBackedValueSupport[Id[Any]] {
-    override def toString(value: Id[Any]): String = value.value
-
-    override def fromString(s: String): Id[Any] = Id[Any](s)
-  }
-
-  implicit def idSupport[D]: ValueSupport[Id[D]] = _idSupport.asInstanceOf[ValueSupport[Id[D]]]
-
-  implicit class FieldExtras[D <: Document[D], F](field: Field[D, F]) {
-    def indexed(fieldType: FieldType = FieldType.Stored,
-                fullTextSearchable: Boolean = true,     // TODO: use default from collection?
-                sortable: Boolean = true)(implicit vs: ValueSupport[F]): Field[D, F] = {
-      field.withFeature(IndexFeature[F](fieldType, fullTextSearchable, sortable, vs))
-    }
-  }
-}
+//package lightdb.index
+//
+//import lightdb.{Document, Id}
+//import lightdb.field.Field
+//
+//package object lucene {
+//  private object _idSupport extends StringBackedValueSupport[Id[Any]] {
+//    override def toString(value: Id[Any]): String = value.value
+//
+//    override def fromString(s: String): Id[Any] = Id[Any](s)
+//  }
+//
+//  implicit def idSupport[D]: ValueSupport[Id[D]] = _idSupport.asInstanceOf[ValueSupport[Id[D]]]
+//
+//  implicit class FieldExtras[D <: Document[D], F](field: Field[D, F]) {
+//    def indexed(fieldType: FieldType = FieldType.Stored,
+//                fullTextSearchable: Boolean = true,     // TODO: use default from collection?
+//                sortable: Boolean = true)(implicit vs: ValueSupport[F]): Field[D, F] = {
+//      field.withFeature(IndexFeature[F](fieldType, fullTextSearchable, sortable, vs))
+//    }
+//  }
+//}
