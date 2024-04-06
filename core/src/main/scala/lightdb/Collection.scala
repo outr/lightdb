20fd5dbf537fccfc54409570110141fb84de13e6
Matt Hicks <matt@matthicks.com>
2024-04-06 10:57:28 -0500

    Began defining triggers

--- core/src/main/scala/lightdb/Collection.scala
+++ core/src/main/scala/lightdb/Collection.scala
@@ -1,5 +1,6 @@
 package lightdb
 
+import cats.effect.IO
 import fabric.rw.RW
 
 abstract class Collection[D <: Document[D]](implicit val rw: RW[D]) {
@@ -10,5 +11,46 @@ abstract class Collection[D <: Document[D]](implicit val rw: RW[D]) {
   protected def db: LightDB
   protected def collectionName: String = defaultCollectionName
 
+  object triggers {
+    lazy val beforeSet: Triggers[BeforeSet[D]] = new Triggers
+    lazy val afterSet: Triggers[AfterSet[D]] = new Triggers
+    lazy val beforeDelete: Triggers[BeforeDelete[D]] = new Triggers
+    lazy val afterDelete: Triggers[AfterDelete[D]] = new Triggers
+  }
   // TODO: Triggers
+  // TODO: set, modify, delete, get, apply
+}
+
+class Triggers[T] {
+  private var _list = List.empty[T]
+  def list: List[T] = _list
+
+  def add(triggers: T*): Unit = synchronized {
+    _list = _list ::: triggers.toList
+  }
+
+  def remove(triggers: T*): Unit = synchronized {
+    val set = triggers.toSet
+    _list = _list.filterNot(t => set.contains(t))
+  }
+
+  def +=(trigger: T): Unit = add(trigger)
+
+  def -=(trigger: T): Unit = remove(trigger)
+}
+
+trait BeforeSet[D <: Document[D]] {
+  def apply(document: D): IO[Option[D]]
+}
+
+trait AfterSet[D <: Document[D]] {
+  def apply(document: D): IO[Unit]
+}
+
+trait BeforeDelete[D <: Document[D]] {
+  def apply(id: Id[D]): IO[Option[Id[D]]]
+}
+
+trait AfterDelete[D <: Document[D]] {
+  def apply(id: Id[D]): IO[Unit]
 }
