8f71dad0ce1498cb59b71c5415a0fbc0b9364a18
Matt Hicks <matt@outr.com>
2024-04-07 20:10:25 -0500

    Added SQLite

--- benchmark/src/main/scala/benchmark/IMDBBenchmark.scala
+++ benchmark/src/main/scala/benchmark/IMDBBenchmark.scala
@@ -50,7 +50,7 @@ import java.util.concurrent.atomic.{AtomicBoolean, AtomicInteger}
  */
 object IMDBBenchmark { // extends IOApp {
   implicit val runtime: IORuntime = IORuntime.global
-  val implementation: BenchmarkImplementation = MariaDBImplementation
+  val implementation: BenchmarkImplementation = SQLiteImplementation
 
   private var ids: List[Ids] = Nil
 
