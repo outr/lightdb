574e5b6664e1c6eeed0d746a3374837bdb512897
Matt Hicks <matt@matthicks.com>
2024-04-09 20:14:33 -0500

    Cleanup

--- core/src/main/scala/lightdb/index/Indexer.scala
+++ core/src/main/scala/lightdb/index/Indexer.scala
@@ -5,13 +5,21 @@ import lightdb.query.SearchContext
 import lightdb.{Collection, Document, Id, query}
 
 trait Indexer[D <: Document[D]] {
-  def indexSupport: IndexSupport[D]
+  protected var _fields = List.empty[IndexedField[_, D]]
 
-  private[lightdb] def delete(id: Id[D]): IO[Unit]
+  def fields: List[IndexedField[_, D]] = _fields
+
+  def indexSupport: IndexSupport[D]
 
   def commit(): IO[Unit]
 
   def count(): IO[Int]
 
   def withSearchContext[Return](f: SearchContext[D] => IO[Return]): IO[Return]
+
+  protected[lightdb] def register[F](field: IndexedField[F, D]): Unit = synchronized {
+    _fields = field :: _fields
+  }
+
+  private[lightdb] def delete(id: Id[D]): IO[Unit]
 }
