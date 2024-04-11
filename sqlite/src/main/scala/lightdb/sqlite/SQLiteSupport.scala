50a982ea04ebfad3f50eb7c8ebf3fcf2b15abd8e
Matt Hicks <matt@matthicks.com>
2024-04-10 20:17:48 -0500

    SQLite support effort

--- sqlite/src/main/scala/lightdb/sqlite/SQLiteSupport.scala
+++ sqlite/src/main/scala/lightdb/sqlite/SQLiteSupport.scala
@@ -5,16 +5,31 @@ import lightdb.{Document, Id}
 import lightdb.index.{IndexSupport, Indexer}
 import lightdb.query.{PagedResults, Query, SearchContext}
 
+import java.nio.file.{Files, Path}
+import java.sql.{Connection, DriverManager}
+
 trait SQLiteSupport[D <: Document[D]] extends IndexSupport[D] {
+  private lazy val path: Path = db.directory.resolve("sqlite.db")
+  // TODO: Should each collection have a connection?
+  private lazy val connection: Connection = {
+    val c = DriverManager.getConnection(s"jdbc:sqlite:${path.toFile.getCanonicalPath}")
+    c.setAutoCommit(false)
+    c
+  }
+
   override lazy val index: SQLiteIndexer[D] = SQLiteIndexer(this)
 
   override def doSearch(query: Query[D],
                         context: SearchContext[D],
                         offset: Int,
                         after: Option[PagedResults[D]]): IO[PagedResults[D]] = ???
+
+  override def dispose(): IO[Unit] = super.dispose().map { _ =>
+    connection.close()
+  }
 }
 
-case class SQLiteIndexer[D <: Document[D]](indexSupport: IndexSupport[D]) extends Indexer[D] {
+case class SQLiteIndexer[D <: Document[D]](indexSupport: SQLiteSupport[D]) extends Indexer[D] {
   override def withSearchContext[Return](f: SearchContext[D] => IO[Return]): IO[Return] = ???
 
   override def count(): IO[Int] = ???
@@ -22,4 +37,6 @@ case class SQLiteIndexer[D <: Document[D]](indexSupport: IndexSupport[D]) extend
   override private[lightdb] def delete(id: Id[D]): IO[Unit] = ???
 
   override def commit(): IO[Unit] = ???
+
+
 }
