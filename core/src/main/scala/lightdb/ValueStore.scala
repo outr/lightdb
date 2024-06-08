9a37b4fcf9b543183fb3887d3b00036e6b0759e4
Matt Hicks <matt@outr.com>
2024-06-08 16:04:31 -0500

    Fixes for Scala 3

--- core/src/main/scala/lightdb/ValueStore.scala
+++ core/src/main/scala/lightdb/ValueStore.scala
@@ -2,7 +2,9 @@ package lightdb
 
 import cats.effect.IO
 import cats.implicits.toTraverseOps
-import fabric.rw.RW
+import fabric.define.DefType
+import fabric.{arr, obj}
+import fabric.rw._
 import lightdb.model.{AbstractCollection, DocumentAction}
 
 case class ValueStore[V, D <: Document[D]](key: String,
@@ -11,17 +13,15 @@ case class ValueStore[V, D <: Document[D]](key: String,
                                            includeIds: Boolean = false,
                                            persistence: Persistence = Persistence.Stored)
                                           (implicit rw: RW[V]) {
-  private implicit val facetRW: RW[Facet] = RW.gen
-
-  private lazy val stored = collection.db.stored[Map[V, Facet]](
+  private lazy val stored = collection.db.stored[Map[V, Facet[D]]](
     key = s"${collection.collectionName}.valueStore.$key",
     default = Map.empty,
     persistence = persistence
   )
 
-  def facets: IO[Map[V, Facet]] = stored.get()
+  def facets: IO[Map[V, Facet[D]]] = stored.get()
 
-  def facet(v: V): IO[Facet] = facets.map(_.getOrElse(v, Facet()))
+  def facet(v: V): IO[Facet[D]] = facets.map(_.getOrElse(v, Facet()))
 
   def values: IO[Set[V]] = facets.map(_.keySet)
 
@@ -52,6 +52,21 @@ case class ValueStore[V, D <: Document[D]](key: String,
     }.sequence.map(_ => Some(doc))
   })
   collection.truncateActions += stored.clear()
+}
 
-  case class Facet(count: Int = 0, ids: Set[Id[D]] = Set.empty[Id[D]])
+case class Facet[D <: Document[D]](count: Int = 0, ids: Set[Id[D]] = Set.empty[Id[D]])
+
+object Facet {
+  implicit def rw[D <: Document[D]]: RW[Facet[D]] = RW.from[Facet[D]](
+    r = f => obj("count" -> f.count.json, "ids" -> arr(f.ids.toList.map(_.json): _*)),
+    w = j => Facet[D](
+      count = j("count").asInt,
+      ids = j("ids").asVector.map(_.asString).map(Id.apply[D]).toSet
+    ),
+    d = DefType.Obj(
+      Some("lightdb.Facet"),
+      "count" -> DefType.Int,
+      "ids" -> DefType.Arr(DefType.Str)
+    )
+  )
 }
