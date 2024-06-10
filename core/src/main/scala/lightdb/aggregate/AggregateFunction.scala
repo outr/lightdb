25d107b92f0954ac482cfe1619ed52cc0d400326
Matt Hicks <matt@outr.com>
2024-06-10 07:41:05 -0500

    Aggregation WIP

--- core/src/main/scala/lightdb/aggregate/AggregateFunction.scala
+++ core/src/main/scala/lightdb/aggregate/AggregateFunction.scala
@@ -2,5 +2,7 @@ package lightdb.aggregate
 
 import fabric.rw.RW
 import lightdb.Document
+import lightdb.index.Index
 
-case class AggregateFunction[F, D <: Document[D]](name: String, fieldName: String, `type`: AggregateType)(implicit val rw: RW[F])
+case class AggregateFunction[T, F, D <: Document[D]](name: String, index: Index[F, D], `type`: AggregateType)
+                                                    (implicit val rw: RW[T])
--- core/src/main/scala/lightdb/index/Materialized.scala
+++ core/src/main/scala/lightdb/index/Materialized.scala
@@ -12,10 +12,10 @@ case class Materialized[D <: Document[D]](json: Json) {
     case t: Throwable => throw new RuntimeException(s"Failed to materialize $name, JSON: $json", t)
   }
   def get[F](index: Index[F, D]): Option[F] = get(index.fieldName, index.rw)
-  def get[F](function: AggregateFunction[F, D]): Option[F] = get(function.name, function.rw)
+  def get[T, F](function: AggregateFunction[T, F, D]): Option[T] = get(function.name, function.rw)
 
   def apply[F](index: Index[F, D]): F = get(index).getOrElse(throw new NullPointerException(s"${index.fieldName} not found in $json"))
-  def apply[F](function: AggregateFunction[F, D]): F = get(function).getOrElse(throw new NullPointerException(s"${function.name} not found in $json"))
+  def apply[T, F](function: AggregateFunction[T, F, D]): T = get(function).getOrElse(throw new NullPointerException(s"${function.name} not found in $json"))
 
   def as[T](implicit rw: RW[T]): T = json.as[T]
 }
