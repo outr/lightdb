530b73d8542a9ddfa468fe21939bfb424027c75c
Matt Hicks <matt@matthicks.com>
2024-06-15 08:34:32 -0500

    WIP

--- next/src/main/scala/lightdb/document/DocumentModel.scala
+++ next/src/main/scala/lightdb/document/DocumentModel.scala
@@ -8,9 +8,9 @@ import lightdb.collection.Collection
 trait DocumentModel[D <: Document[D]] {
   // TODO: listeners
 
-  private[lightdb] def init(collection: Collection[D]): IO[Unit] = {
-
-  }
+//  private[lightdb] def init(collection: Collection[D]): IO[Unit] = {
+//
+//  }
 
   def id(value: String = Unique()): Id[D] = Id(value)
 }
