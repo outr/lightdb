25b4d4f63af26b2bc2cdc8861a4a3d5bb1287672
Matt Hicks <matt@outr.com>
2024-06-23 08:54:51 -0500

    WIP

---
+++ sql/src/main/scala/lightdb/sql/SQLQueryBuilder.scala
@@ -0,0 +1,83 @@
+package lightdb.sql
+
+import lightdb.collection.Collection
+import lightdb.document.Document
+
+import java.sql.{Connection, ResultSet}
+
+case class SQLQueryBuilder[D <: Document[D]](collection: Collection[D, _],
+                                             fields: List[SQLPart] = Nil,
+                                             filters: List[SQLPart] = Nil,
+                                             group: List[SQLPart] = Nil,
+                                             having: List[SQLPart] = Nil,
+                                             sort: List[SQLPart] = Nil,
+                                             limit: Option[Int] = None,
+                                             offset: Int) {
+  def queryTotal(connection: Connection): Int = {
+    val b = copy(
+      fields = List(SQLPart("COUNT(*) AS count", Nil)),
+      group = Nil,
+      having = Nil,
+      sort = Nil,
+      limit = None,
+      offset = 0
+    )
+    val rs = b.execute(connection)
+    try {
+      rs.next()
+      rs.getInt(1)
+    } finally {
+      rs.close()
+    }
+  }
+
+  def execute(connection: Connection): ResultSet = {
+    val b = new StringBuilder
+    b.append("SELECT\n")
+    b.append(s"\t${fields.map(_.sql).mkString(", ")}\n")
+    b.append("FROM\n")
+    b.append(s"\t${collection.name}\n")
+    filters.zipWithIndex.foreach {
+      case (f, index) =>
+        if (index == 0) {
+          b.append("WHERE\n")
+        } else {
+          b.append("AND\n")
+        }
+        b.append(s"\t${f.sql}\n")
+    }
+    if (group.nonEmpty) {
+      b.append("GROUP BY\n\t")
+      b.append(group.map(_.sql).mkString(", "))
+      b.append('\n')
+    }
+    having.zipWithIndex.foreach {
+      case (f, index) =>
+        if (index == 0) {
+          b.append("HAVING\n")
+        } else {
+          b.append("AND\n")
+        }
+        b.append(s"\t${f.sql}\n")
+    }
+    if (sort.nonEmpty) {
+      b.append("ORDER BY\n\t")
+      b.append(sort.map(_.sql).mkString(", "))
+      b.append('\n')
+    }
+    limit.foreach { l =>
+      b.append("LIMIT\n")
+      b.append(s"\t$l\n")
+    }
+    if (offset > 0) {
+      b.append("OFFSET\n")
+      b.append(s"\t$offset\n")
+    }
+    val ps = connection.prepareStatement(b.toString())
+    val args = (fields ::: filters ::: sort).flatMap(_.args)
+    args.zipWithIndex.foreach {
+      case (value, index) => SQLIndexer.setValue(ps, index + 1, value)
+    }
+    ps.executeQuery()
+  }
+}
