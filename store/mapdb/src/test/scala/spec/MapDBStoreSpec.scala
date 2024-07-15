38dabefb5d9f852a6a295e25eb69cfbe859f01d6
Matt Hicks <matt@outr.com>
2024-07-15 09:28:56 -0500

    Added MapDBStore back in

--- store/mapdb/src/test/scala/spec/MapDBStoreSpec.scala
+++
@@ -1,8 +0,0 @@
-package spec
-
-import lightdb.mapdb.MapDBStore
-import lightdb.store.StoreManager
-
-class MapDBStoreSpec extends AbstractStoreSpec {
-  override protected def storeManager: StoreManager = MapDBStore
-}
