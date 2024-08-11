d087396426dceb95f798a809c14eac3b68e02d1d
Matt Hicks <matt@outr.com>
2024-08-11 10:33:28 -0500

    Minor fixes and improvements

--- async/src/main/scala/lightdb/async/AsyncSearchResults.scala
+++ async/src/main/scala/lightdb/async/AsyncSearchResults.scala
@@ -5,10 +5,11 @@ import lightdb.doc.Document
 import lightdb.transaction.Transaction
 
 case class AsyncSearchResults[Doc <: Document[Doc], V](offset: Int,
-                                      limit: Option[Int],
-                                      total: Option[Int],
-                                      stream: fs2.Stream[IO, V],
-                                      transaction: Transaction[Doc]) {
+                                                       limit: Option[Int],
+                                                       total: Option[Int],
+                                                       scoredStream: fs2.Stream[IO, (V, Double)],
+                                                       transaction: Transaction[Doc]) {
+  def stream: fs2.Stream[IO, V] = scoredStream.map(_._1)
   def first: IO[Option[V]] = stream.take(1).compile.toList.map(_.headOption)
   def one: IO[V] = first.map {
     case Some(v) => v
