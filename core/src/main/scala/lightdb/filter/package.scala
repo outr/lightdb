3d930701a21b7df9a93ac6da3fcc944d2bb47bc7
Matt Hicks <matt@outr.com>
2024-08-21 12:50:37 -0500

    Disabled running tests on GH temporarily

--- core/src/main/scala/lightdb/filter/package.scala
+++ core/src/main/scala/lightdb/filter/package.scala
@@ -2,11 +2,9 @@ package lightdb
 
 package object filter {
   implicit class ListFilterExtras[V, Doc, Filter](fs: FilterSupport[List[V], Doc, Filter]) {
-    def ===(value: V): Filter = fs.is(List(value))
-    def is(value: V): Filter = fs.is(List(value))
+    def has(value: V): Filter = fs.is(List(value))
   }
   implicit class SetFilterExtras[V, Doc, Filter](fs: FilterSupport[Set[V], Doc, Filter]) {
-    def ===(value: V): Filter = fs.is(Set(value))
-    def is(value: V): Filter = fs.is(Set(value))
+    def has(value: V): Filter = fs.is(Set(value))
   }
 }
