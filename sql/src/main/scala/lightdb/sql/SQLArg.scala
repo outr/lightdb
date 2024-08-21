0010bb19c336eecb432ce8d819af6d116c3a3cb2
Matt Hicks <matt@outr.com>
2024-08-21 12:21:14 -0500

    Updates and fixes to SQL support

--- sql/src/main/scala/lightdb/sql/SQLArg.scala
+++ sql/src/main/scala/lightdb/sql/SQLArg.scala
@@ -15,30 +15,32 @@ trait SQLArg {
 
 object SQLArg {
   case class FieldArg[Doc, F](field: Field[Doc, F], value: F) extends SQLArg {
-    private def setInternal(ps: PreparedStatement, index: Int, value: Any): Unit = value match {
-      case null => ps.setNull(index, Types.NULL)
-      case _ if field.isInstanceOf[Tokenized[_]] =>
-        val s = value.toString
-        ps.setString(index, s.toLowerCase.split("\\s+").filterNot(_.isEmpty).mkString(" "))
-      case Some(value) => setInternal(ps, index, value)
-      case None => ps.setNull(index, Types.NULL)
-      case id: Id[_] => ps.setString(index, id.value)
-      case s: String => ps.setString(index, s)
-      case b: Boolean => ps.setInt(index, if (b) 1 else 0)
-      case i: Int => ps.setInt(index, i)
-      case l: Long => ps.setLong(index, l)
-      case f: Float => ps.setFloat(index, f)
-      case d: Double => ps.setDouble(index, d)
-      case json: Json => ps.setString(index, JsonFormatter.Compact(json))
-      case point: GeoPoint => ps.setString(index, s"POINT(${point.longitude} ${point.latitude})")
-      case _ =>
-        val json = value.asInstanceOf[F].json(field.rw)
-        val string = if (field.rw.definition == DefType.Str) {
-          json.asString
-        } else {
-          JsonFormatter.Compact(json)
-        }
-        ps.setString(index, string)
+    private def setInternal(ps: PreparedStatement, index: Int, value: Any): Unit = {
+      scribe.trace(s"SQLArg: $index / $value (${if (value != null) value.getClass.getName else "null"})")
+      value match {
+        case null | None => ps.setNull(index, Types.NULL)
+        case _ if field.isInstanceOf[Tokenized[_]] =>
+          val s = value.toString
+          ps.setString(index, s.toLowerCase.split("\\s+").filterNot(_.isEmpty).mkString(" "))
+        case Some(value) => setInternal(ps, index, value)
+        case id: Id[_] => ps.setString(index, id.value)
+        case s: String => ps.setString(index, s)
+        case b: Boolean => ps.setInt(index, if (b) 1 else 0)
+        case i: Int => ps.setInt(index, i)
+        case l: Long => ps.setLong(index, l)
+        case f: Float => ps.setFloat(index, f)
+        case d: Double => ps.setDouble(index, d)
+        case json: Json => ps.setString(index, JsonFormatter.Compact(json))
+        case point: GeoPoint => ps.setString(index, s"POINT(${point.longitude} ${point.latitude})")
+        case _ =>
+          val json = value.asInstanceOf[F].json(field.rw)
+          val string = if (field.rw.definition == DefType.Str) {
+            json.asString
+          } else {
+            JsonFormatter.Compact(json)
+          }
+          ps.setString(index, string)
+      }
     }
 
     override def set(ps: PreparedStatement, index: Int): Unit = setInternal(ps, index, value)
