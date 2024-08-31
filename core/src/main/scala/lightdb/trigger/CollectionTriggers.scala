bf5d689cc12b741d16ad4fa813b38ac507f113e8
Matt Hicks <matt@outr.com>
2024-08-31 07:14:54 -0500

    Added support for triggers

---
+++ core/src/main/scala/lightdb/trigger/CollectionTriggers.scala
@@ -0,0 +1,32 @@
+package lightdb.trigger
+
+import lightdb.UniqueIndex
+import lightdb.doc.Document
+import lightdb.transaction.Transaction
+
+class CollectionTriggers[Doc <: Document[Doc]] extends CollectionTrigger[Doc] {
+  private var list = List.empty[CollectionTrigger[Doc]]
+
+  def +=(trigger: CollectionTrigger[Doc]): Unit = synchronized {
+    list = trigger :: list
+  }
+
+  def -=(trigger: CollectionTrigger[Doc]): Unit = synchronized {
+    list = list.filterNot(_ eq trigger)
+  }
+
+  override def insert(doc: Doc)(implicit transaction: Transaction[Doc]): Unit =
+    list.foreach(_.insert(doc))
+
+  override def upsert(doc: Doc)(implicit transaction: Transaction[Doc]): Unit =
+    list.foreach(_.upsert(doc))
+
+  override def delete[V](index: UniqueIndex[Doc, V], value: V)(implicit transaction: Transaction[Doc]): Unit =
+    list.foreach(_.delete(index, value))
+
+  override def truncate(): Unit =
+    list.foreach(_.truncate())
+
+  override def dispose(): Unit =
+    list.foreach(_.dispose())
+}
