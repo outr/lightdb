8ec9cc2986721070fba9d67a65566fdcc48d10bb
Matt Hicks <matt@outr.com>
2024-09-23 16:04:41 -0500

    Release 0.13.1 - Fixed async support for facets

--- async/src/main/scala/lightdb/async/AsyncSearchResults.scala
+++ async/src/main/scala/lightdb/async/AsyncSearchResults.scala
@@ -1,18 +1,28 @@
 package lightdb.async
 
 import cats.effect.IO
-import lightdb.doc.Document
+import lightdb.doc.{Document, DocumentModel}
+import lightdb.facet.FacetResult
+import lightdb.field.Field.FacetField
 import lightdb.transaction.Transaction
 
-case class AsyncSearchResults[Doc <: Document[Doc], V](offset: Int,
-                                                       limit: Option[Int],
-                                                       total: Option[Int],
-                                                       scoredStream: fs2.Stream[IO, (V, Double)],
-                                                       transaction: Transaction[Doc]) {
+case class AsyncSearchResults[Doc <: Document[Doc], Model <: DocumentModel[Doc], V](model: Model,
+                                                                                    offset: Int,
+                                                                                    limit: Option[Int],
+                                                                                    total: Option[Int],
+                                                                                    scoredStream: fs2.Stream[IO, (V, Double)],
+                                                                                    facetResults: Map[FacetField[Doc], FacetResult],
+                                                                                    transaction: Transaction[Doc]) {
   def stream: fs2.Stream[IO, V] = scoredStream.map(_._1)
+
   def first: IO[Option[V]] = stream.take(1).compile.toList.map(_.headOption)
+
   def one: IO[V] = first.map {
     case Some(v) => v
     case None => throw new NullPointerException("No results for search")
   }
+
+  def facet(f: Model => FacetField[Doc]): FacetResult = facetResults(f(model))
+
+  def getFacet(f: Model => FacetField[Doc]): Option[FacetResult] = facetResults.get(f(model))
 }
