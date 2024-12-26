d5f79f9552650b91a6d4fe5129004bc7cb9c58d8
Matt Hicks <matt@outr.com>
2024-12-26 08:53:51 -0600

    Updated Sort.BestMatch to support Ascending

--- core/src/main/scala/lightdb/Sort.scala
+++ core/src/main/scala/lightdb/Sort.scala
@@ -7,19 +7,21 @@ import lightdb.spatial.Geo
 trait Sort
 
 object Sort {
-  case object BestMatch extends Sort
+  case class BestMatch(direction: SortDirection = SortDirection.Descending) extends Sort {
+    def direction(direction: SortDirection): BestMatch = copy(direction = direction)
+    def ascending: BestMatch = direction(SortDirection.Ascending)
+    def asc: BestMatch = direction(SortDirection.Ascending)
+    def descending: BestMatch = direction(SortDirection.Descending)
+    def desc: BestMatch = direction(SortDirection.Descending)
+  }
 
   case object IndexOrder extends Sort
 
   case class ByField[Doc <: Document[Doc], F](field: Field[Doc, F], direction: SortDirection = SortDirection.Ascending) extends Sort {
     def direction(direction: SortDirection): ByField[Doc, F] = copy(direction = direction)
-
     def ascending: ByField[Doc, F] = direction(SortDirection.Ascending)
-
     def asc: ByField[Doc, F] = direction(SortDirection.Ascending)
-
     def descending: ByField[Doc, F] = direction(SortDirection.Descending)
-
     def desc: ByField[Doc, F] = direction(SortDirection.Descending)
   }
 
@@ -27,13 +29,9 @@ object Sort {
                              from: Geo.Point,
                              direction: SortDirection = SortDirection.Ascending) extends Sort {
     def direction(direction: SortDirection): ByDistance[Doc, G] = copy(direction = direction)
-
     def ascending: ByDistance[Doc, G] = direction(SortDirection.Ascending)
-
     def asc: ByDistance[Doc, G] = direction(SortDirection.Ascending)
-
     def descending: ByDistance[Doc, G] = direction(SortDirection.Descending)
-
     def desc: ByDistance[Doc, G] = direction(SortDirection.Descending)
   }
 }
