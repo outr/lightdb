1c75a7e0e98dab68a821a10902d5c1c7a654377c
Matt Hicks <matt@outr.com>
2025-02-03 08:05:24 -0600

    Updates to Timestamp functionality

--- core/src/main/scala/lightdb/Timestamp.scala
+++ core/src/main/scala/lightdb/Timestamp.scala
@@ -2,8 +2,19 @@ package lightdb
 
 import fabric.define.DefType
 import fabric.rw._
+import perfolation.long2Implicits
 
-case class Timestamp(value: Long = System.currentTimeMillis()) extends AnyVal
+import java.util.Calendar
+
+case class Timestamp(value: Long = System.currentTimeMillis()) extends AnyVal {
+  def year: Int = value.t.year
+  def month: Int = value.t.month + 1
+  def day: Int = value.t.dayOfMonth
+  def hour: Int = value.t.hour24
+  def minute: Int = value.t.minuteOfHour
+  def second: Int = value.t.secondOfMinute
+  def millisecond: Int = value.t.milliOfSecond
+}
 
 object Timestamp {
   implicit val rw: RW[Timestamp] = RW.from(
@@ -12,4 +23,21 @@ object Timestamp {
     d = DefType.Int
   )
   implicit val numeric: Numeric[Timestamp] = Numeric[Long].map(Timestamp.apply)(_.value)
+
+  def fromCalendar(calendar: Calendar): Timestamp = Timestamp(calendar.getTimeInMillis)
+
+  def of(year: Int = now.year,
+         month: Int = now.month,
+         day: Int = now.day,
+         hour: Int = 0,
+         minute: Int = 0,
+         second: Int = 0,
+         millisecond: Int = 0): Timestamp = {
+    val c = Calendar.getInstance()
+    c.set(Calendar.MILLISECOND, millisecond)
+    c.set(year, month - 1, day, hour, minute, second)
+    fromCalendar(c)
+  }
+
+  def now: Timestamp = Timestamp()
 }
