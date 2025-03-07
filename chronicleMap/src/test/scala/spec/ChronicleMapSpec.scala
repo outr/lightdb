b5a0a51b64d882afc24dd3a55b0654c8f98f3997
Matt Hicks <matt@outr.com>
2025-03-07 16:54:23 -0600

    Added ChronicleMapStore

---
+++ chronicleMap/src/test/scala/spec/ChronicleMapSpec.scala
@@ -0,0 +1,9 @@
+package spec
+
+import lightdb.chroniclemap.ChronicleMapStore
+import lightdb.store.StoreManager
+
+@EmbeddedTest
+class ChronicleMapSpec extends AbstractKeyValueSpec {
+  override def storeManager: StoreManager = ChronicleMapStore
+}
