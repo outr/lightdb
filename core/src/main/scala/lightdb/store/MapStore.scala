8c3684bbac7e11bcbb9981d130dd4f01c3408c0b
Matt Hicks <matt@outr.com>
2025-03-30 09:06:43 -0500

    Minor updates and improvements

--- core/src/main/scala/lightdb/store/MapStore.scala
+++ core/src/main/scala/lightdb/store/MapStore.scala
@@ -15,24 +15,26 @@ class MapStore[Doc <: Document[Doc], Model <: DocumentModel[Doc]](name: String,
                                                                   val storeMode: StoreMode[Doc, Model],
                                                                   db: LightDB,
                                                                   storeManager: StoreManager) extends Store[Doc, Model](name, model, db, storeManager) { store =>
-  private var map = Map.empty[Id[Doc], Doc]
+  private var _map = Map.empty[Id[Doc], Doc]
+
+  def map: Map[Id[Doc], Doc] = _map
 
   override def prepareTransaction(transaction: Transaction[Doc]): Task[Unit] = Task.unit
 
   override protected def _insert(doc: Doc)(implicit transaction: Transaction[Doc]): Task[Doc] = Task {
     store.synchronized {
-      map += id(doc) -> doc
+      _map += id(doc) -> doc
     }
     doc
   }
 
   override protected def _upsert(doc: Doc)(implicit transaction: Transaction[Doc]): Task[Doc] = _insert(doc)
 
-  override def exists(id: Id[Doc])(implicit transaction: Transaction[Doc]): Task[Boolean] = Task(map.contains(id))
+  override def exists(id: Id[Doc])(implicit transaction: Transaction[Doc]): Task[Boolean] = Task(_map.contains(id))
 
   override protected def _get[V](field: UniqueIndex[Doc, V], value: V)(implicit transaction: Transaction[Doc]): Task[Option[Doc]] = Task {
     if (field == idField) {
-      map.get(value.asInstanceOf[Id[Doc]])
+      _map.get(value.asInstanceOf[Id[Doc]])
     } else {
       throw new UnsupportedOperationException(s"MapStore can only get on _id, but ${field.name} was attempted")
     }
@@ -43,8 +45,8 @@ class MapStore[Doc <: Document[Doc], Model <: DocumentModel[Doc]](name: String,
     store.synchronized {
       if (field == idField) {
         val id = value.asInstanceOf[Id[Doc]]
-        val contains = map.contains(id)
-        map -= id
+        val contains = _map.contains(id)
+        _map -= id
         contains
       } else {
         throw new UnsupportedOperationException(s"MapStore can only get on _id, but ${field.name} was attempted")
@@ -53,14 +55,14 @@ class MapStore[Doc <: Document[Doc], Model <: DocumentModel[Doc]](name: String,
   }
 
   override def count(implicit transaction: Transaction[Doc]): Task[Int] = Task {
-    map.size
+    _map.size
   }
 
   override def jsonStream(implicit transaction: Transaction[Doc]): rapid.Stream[Json] =
-    rapid.Stream.fromIterator(Task(map.valuesIterator.map(_.json(model.rw))))
+    rapid.Stream.fromIterator(Task(_map.valuesIterator.map(_.json(model.rw))))
 
   override def stream(implicit transaction: Transaction[Doc]): rapid.Stream[Doc] =
-    rapid.Stream.fromIterator(Task(map.valuesIterator))
+    rapid.Stream.fromIterator(Task(_map.valuesIterator))
 
   override def doSearch[V](query: Query[Doc, Model, V])
                           (implicit transaction: Transaction[Doc]): Task[SearchResults[Doc, Model, V]] = throw new UnsupportedOperationException("MapStore does not support searching")
@@ -72,15 +74,15 @@ class MapStore[Doc <: Document[Doc], Model <: DocumentModel[Doc]](name: String,
 
   override def truncate()(implicit transaction: Transaction[Doc]): Task[Int] = Task {
     store.synchronized {
-      val size = map.size
-      map = Map.empty
+      val size = _map.size
+      _map = Map.empty
       size
     }
   }
 
   override protected def doDispose(): Task[Unit] = Task {
     store.synchronized {
-      map = Map.empty
+      _map = Map.empty
     }
   }
 }
--- core/src/test/scala/spec/TypeSafetySpec.scala
+++ core/src/test/scala/spec/TypeSafetySpec.scala
@@ -3,6 +3,7 @@ package spec
 import fabric.rw._
 import lightdb.{Id, LightDB, store}
 import lightdb.doc.{Document, DocumentModel, JsonConversion}
+import lightdb.field.Field
 import lightdb.store.{MapStore, Store, StoreManager}
 import lightdb.upgrade.DatabaseUpgrade
 import org.scalatest.matchers.should.Matchers
@@ -22,20 +23,18 @@ class TypeSafetySpec extends AnyWordSpec with Matchers {
         override val storeManager: MapStore.type = MapStore
         override def upgrades: List[DatabaseUpgrade] = Nil
 
-        // This should return a MapStore[Person, Person.type], not a generic Store[Person, Person.type]
         lazy val people: MapStore[Person, Person.type] = store[Person, Person.type](Person)
       }
 
-      // Initialize the database
       TestDB.init.sync()
 
-      // Verify that the store is of the correct type
       TestDB.people shouldBe a[MapStore[_, _]]
 
-      // This should compile because people is a MapStore, not a generic Store
-      val mapStore: MapStore[Person, Person.type] = TestDB.people
+      TestDB.people.t.insert(Person("Test 1", 1)).sync()
 
-      // Clean up
+      val mapStore: MapStore[Person, Person.type] = TestDB.people
+      mapStore.map.size should be(1)
+
       TestDB.dispose.sync()
     }
   }
@@ -45,7 +44,7 @@ class TypeSafetySpec extends AnyWordSpec with Matchers {
   object Person extends DocumentModel[Person] with JsonConversion[Person] {
     override implicit val rw: RW[Person] = RW.gen
 
-    val name = field("name", _.name)
-    val age = field("age", _.age)
+    val name: F[String] = field("name", _.name)
+    val age: F[Int] = field("age", _.age)
   }
 }
