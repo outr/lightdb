8db7011f647b602562789f6766f964b0438a39cd
Matt Hicks <matt@outr.com>
2025-04-24 16:00:48 -0500

    Added more advanced unit testing scenario

---
+++ halodb/src/test/scala/spec/HaloDBEmployeeInfluenceSpec.scala
@@ -0,0 +1,9 @@
+package spec
+
+import lightdb.halodb.HaloDBStore
+import lightdb.store.StoreManager
+
+@EmbeddedTest
+class HaloDBEmployeeInfluenceSpec extends AbstractEmployeeInfluenceSpec {
+  override def storeManager: StoreManager = HaloDBStore
+}
