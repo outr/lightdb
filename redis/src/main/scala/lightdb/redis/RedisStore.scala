9c0b1f782adec9d4fea577ce46218faac1d4bd5e
Matt Hicks <matt@outr.com>
2025-05-03 11:11:11 -0500

    Fixed Redis support

--- redis/src/main/scala/lightdb/redis/RedisStore.scala
+++ redis/src/main/scala/lightdb/redis/RedisStore.scala
@@ -1,16 +1,13 @@
 package lightdb.redis
 
-import _root_.redis.clients.jedis.{Jedis, JedisPool, JedisPoolConfig}
-import fabric.Json
+import _root_.redis.clients.jedis.{JedisPool, JedisPoolConfig}
 import lightdb._
 import lightdb.doc.{Document, DocumentModel}
-import lightdb.field.Field._
 import lightdb.store.{Store, StoreManager, StoreMode}
-import lightdb.transaction.{Transaction, TransactionFeature, TransactionKey}
-import rapid.{Task, Unique}
+import lightdb.transaction.Transaction
+import rapid._
 
 import java.nio.file.Path
-import scala.jdk.CollectionConverters.IteratorHasAsScala
 
 class RedisStore[Doc <: Document[Doc], Model <: DocumentModel[Doc]](name: String,
                                                                     path: Option[Path],
@@ -20,56 +17,15 @@ class RedisStore[Doc <: Document[Doc], Model <: DocumentModel[Doc]](name: String
                                                                     port: Int = 6379,
                                                                     db: LightDB,
                                                                     storeManager: StoreManager) extends Store[Doc, Model](name, path, model, db, storeManager) {
-  private lazy val InstanceKey: TransactionKey[Jedis] = TransactionKey("redisInstance")
+  override type TX = RedisTransaction[Doc, Model]
 
   private lazy val config = new JedisPoolConfig
-  private lazy val pool = new JedisPool(config, hostname, port)
+  private[redis] lazy val pool = new JedisPool(config, hostname, port)
 
   override protected def initialize(): Task[Unit] = super.initialize().next(Task(pool.preparePool()))
 
-  private def getInstance(implicit transaction: Transaction[Doc]): Jedis =
-    transaction.getOrCreate(InstanceKey, pool.getResource)
-
-  override def prepareTransaction(transaction: Transaction[Doc]): Task[Unit] = Task {
-    transaction.put(TransactionKey(), new TransactionFeature {
-      override def commit(): Task[Unit] = Task {
-        transaction.get(InstanceKey).foreach(jedis => pool.returnResource(jedis))
-      }.next(super.commit())
-    })
-  }
-
-  override protected def _insert(doc: Doc)(implicit transaction: Transaction[Doc]): Task[Doc] = upsert(doc)
-
-  override protected def _upsert(doc: Doc)(implicit transaction: Transaction[Doc]): Task[Doc] = Task {
-    getInstance.hset(name, doc._id.value, toString(doc))
-    doc
-  }
-
-  override def exists(id: Id[Doc])(implicit transaction: Transaction[Doc]): Task[Boolean] =
-    Task(getInstance.hexists(name, id.value))
-
-  override protected def _get[V](field: UniqueIndex[Doc, V], value: V)
-                     (implicit transaction: Transaction[Doc]): Task[Option[Doc]] = Task {
-    if (field == idField) {
-      Option(getInstance.hget(name, value.asInstanceOf[Id[Doc]].value)).map(fromString)
-    } else {
-      throw new UnsupportedOperationException(s"HaloDBStore can only get on _id, but ${field.name} was attempted")
-    }
-  }
-
-  override protected def _delete[V](field: UniqueIndex[Doc, V], value: V)
-                        (implicit transaction: Transaction[Doc]): Task[Boolean] =
-    Task(getInstance.hdel(value.asInstanceOf[Id[Doc]].value) > 0L)
-
-  override def count(implicit transaction: Transaction[Doc]): Task[Int] = Task(getInstance.hlen(name).toInt)
-
-  override def jsonStream(implicit transaction: Transaction[Doc]): rapid.Stream[Json] = rapid.Stream.fromIterator(Task {
-    getInstance.hgetAll(name).values().iterator().asScala.map(toJson)
-  })
-
-  override def truncate()(implicit transaction: Transaction[Doc]): Task[Int] = count.map { size =>
-    getInstance.del(name)
-    size
+  override protected def createTransaction(parent: Option[Transaction[Doc, Model]]): Task[TX] = Task {
+    RedisTransaction(this, pool.getResource, parent)
   }
 
   override protected def doDispose(): Task[Unit] = super.doDispose().next(Task(pool.close()))
