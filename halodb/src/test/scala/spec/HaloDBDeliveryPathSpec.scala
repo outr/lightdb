ca1df93588bab920932a029f56be7956cd035541
Matt Hicks <matt@outr.com>
2025-05-03 20:21:59 -0500

    Making HaloDBDeliveryPathSpec LocalOnly

--- halodb/src/test/scala/spec/HaloDBDeliveryPathSpec.scala
+++ halodb/src/test/scala/spec/HaloDBDeliveryPathSpec.scala
@@ -4,6 +4,7 @@ import lightdb.halodb.HaloDBStore
 import lightdb.store.StoreManager
 
 @EmbeddedTest
+@LocalOnly
 class HaloDBDeliveryPathSpec extends AbstractDeliveryPathSpec {
   override def storeManager: StoreManager = HaloDBStore
 }
