9faa9a32874a2f0dd5abea674384ca4c91518fc6
Matt Hicks <matt@outr.com>
2025-05-14 09:01:54 -0500

    Removed legacy traversal functionality

--- core/src/main/scala/lightdb/transaction/PrefixScanningTransaction.scala
+++ core/src/main/scala/lightdb/transaction/PrefixScanningTransaction.scala
@@ -4,10 +4,9 @@ import fabric._
 import fabric.rw._
 import lightdb.doc.{Document, DocumentModel}
 import lightdb.store.PrefixScanningStore
-import lightdb.traversal.{TransactionTraversalSupport => TransactionTraversalSupportOld}
 import lightdb.traverse.TransactionTraversalSupport
 
-trait PrefixScanningTransaction[Doc <: Document[Doc], Model <: DocumentModel[Doc]] extends Transaction[Doc, Model] with TransactionTraversalSupportOld[Doc, Model] with TransactionTraversalSupport[Doc, Model] {
+trait PrefixScanningTransaction[Doc <: Document[Doc], Model <: DocumentModel[Doc]] extends Transaction[Doc, Model] with TransactionTraversalSupport[Doc, Model] {
   override def store: PrefixScanningStore[Doc, Model]
 
   def jsonPrefixStream(prefix: String): rapid.Stream[Json]
