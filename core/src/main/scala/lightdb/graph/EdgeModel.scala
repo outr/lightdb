d4a323df73f683ae457696902979532651dc2980
Matt Hicks <matt@outr.com>
2025-05-14 11:21:56 -0500

    Code cleanup and dependency update

--- core/src/main/scala/lightdb/graph/EdgeModel.scala
+++ core/src/main/scala/lightdb/graph/EdgeModel.scala
@@ -14,12 +14,4 @@ import scala.language.implicitConversions
 trait EdgeModel[Doc <: EdgeDocument[Doc, From, To], From <: Document[From], To <: Document[To]] extends DocumentModel[Doc] {
   val _from: UniqueIndex[Doc, Id[From]] = field.unique("_from", _._from)
   val _to: UniqueIndex[Doc, Id[To]] = field.unique("_to", _._to)
-
-  def prefix(_from: Id[From], _to: Id[To] = null, extra: String = null): String = if (_to == null) {
-    _from.value
-  } else if (extra == null) {
-    s"${_from.value}-${_to.value}"
-  } else {
-    s"${_from.value}-${_to.value}-$extra"
-  }
 }
