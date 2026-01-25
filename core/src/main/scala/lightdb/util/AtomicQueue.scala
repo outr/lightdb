910fab5fbf16a11ebc0a974b3a9b3c12d30172be
Matt Hicks <matt@outr.com>
2026-01-25 08:23:25 -0600

    Introduced new AtomicQueue

---
+++ core/src/main/scala/lightdb/util/AtomicQueue.scala
@@ -0,0 +1,55 @@
+package lightdb.util
+
+import java.util.concurrent.ConcurrentLinkedQueue
+import java.util.concurrent.atomic.{AtomicInteger, AtomicReference}
+import scala.annotation.tailrec
+import scala.collection.immutable.VectorMap
+import scala.compiletime.uninitialized
+
+class AtomicQueue[T] extends Iterable[T] {
+  private val q = new ConcurrentLinkedQueue[T]
+  private val _size = new AtomicInteger(0)
+
+  def poll(): Option[T] = {
+    val t = q.poll()
+    if t == null then None
+    else {
+      _size.decrementAndGet()
+      Some(t)
+    }
+  }
+
+  def add(t: T): Unit = {
+    q.offer(t)
+    _size.incrementAndGet()
+  }
+
+  def clear(): Unit = {
+    q.clear()
+    _size.set(0)
+  }
+
+  override def size: Int = _size.get()
+
+  override def iterator: Iterator[T] = new Iterator[T] {
+    private var cached: T = uninitialized
+    private var cachedValid = false
+
+    private def ensureCached(): Unit =
+      if !cachedValid then
+        cached = q.poll()
+        if cached != null then _size.decrementAndGet()
+        cachedValid = true
+
+    override def hasNext: Boolean =
+      ensureCached()
+      cached != null
+
+    override def next(): T =
+      ensureCached()
+      val v = cached
+      if v == null then throw new NoSuchElementException("next on empty iterator")
+      cachedValid = false
+      v
+  }
+}
