5df59365d4e682a78e58a1bd8ce7145b2655cba3
Matt Hicks <matt@outr.com>
2026-02-26 08:55:53 -0600

    Fixed GoogleSheetsSpec to warn instead of fail without env vars

--- googleSheets/src/test/scala/spec/GoogleSheetsSpec.scala
+++ googleSheets/src/test/scala/spec/GoogleSheetsSpec.scala
@@ -2,6 +2,7 @@ package spec
 
 import lightdb.googlesheets.GoogleSheetsStore
 import lightdb.store.StoreManager
+import org.scalatest.{Args, Status, SucceededStatus}
 
 @EmbeddedTest
 class GoogleSheetsSpec extends AbstractKeyValueSpec {
@@ -11,8 +12,14 @@ class GoogleSheetsSpec extends AbstractKeyValueSpec {
   private val spreadsheetId: String = sys.env.getOrElse("GOOGLE_SHEETS_SPREADSHEET_ID", "")
   private val credentialsPath: String = sys.env.getOrElse("GOOGLE_SHEETS_CREDENTIALS_PATH", "")
 
-  assume(spreadsheetId.nonEmpty && credentialsPath.nonEmpty,
-    "GOOGLE_SHEETS_SPREADSHEET_ID and GOOGLE_SHEETS_CREDENTIALS_PATH env vars must be set")
-
   override def storeManager: StoreManager = GoogleSheetsStore(spreadsheetId, credentialsPath)
+
+  override def run(testName: Option[String], args: Args): Status = {
+    if (spreadsheetId.isEmpty || credentialsPath.isEmpty) {
+      scribe.warn("Skipping GoogleSheetsSpec: GOOGLE_SHEETS_SPREADSHEET_ID and GOOGLE_SHEETS_CREDENTIALS_PATH env vars must be set")
+      SucceededStatus
+    } else {
+      super.run(testName, args)
+    }
+  }
 }
